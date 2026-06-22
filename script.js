df8a07fae9ac80e0abafb3eac3a217e3f0c2c3a0
rananaveen688-prog <rananaveen688@gmail.com>
2026-06-23 00:12:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.hamburger').click(function () {
+        $('.header_navbar ul').slideToggle('400');
+        $(this).toggleClass('open');
+    });
+    $('.header_navbar ul li').on('click', function () {
+        $('.header_navbar ul').slideUp('300');
+        $('.hamburger').removeClass('open');
+    });
+});
